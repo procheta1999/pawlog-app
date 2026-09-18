7dd563886615d0d83e0a7b44a8afa407daf7c883
Procheta <bhattacharyya.procheta1999@gmail.com>
2026-09-18 18:51:21 +0530

    content refactor

--- src/app/dashboard/today/components/CareScheduleCard.js
+++ src/app/dashboard/today/components/CareScheduleCard.js
@@ -45,7 +45,7 @@ export default function CareScheduleCard({
                     </Typography>
                     <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Before setting up care events, we want to know the care schedule of your pet which will help us setup the care timeline better</Typography>
                     <Typography variant="body2" sx={{ mb: 2 }}>
-                        Please make sure your pet&apos;s stomach is not empty for more than 6-7 hours because that might lead to gastristis. Please consult the vet for further details.
+                        Add your pet's usual care schedule so PawLog can distinguish expected care from recorded observations.
                     </Typography>
                     {timelineItems.length > 0 && (
                         <Timeline items={timelineItems} onEventMenuAction={onEventMenuAction} />
